ff73d5db9a85105910e117b3d82108ff6b442588
maryam <maryamhaaadi8077@gmail.com>
2023-05-19 15:51:12 +0330

    changes3

--- res/account/urls.py
+++ res/account/urls.py
@@ -13,7 +13,6 @@ urlpatterns=[
     path("login_user",views.login_user,name='login_user'),
     path("logout_user",views.logout_user,name='logout_user'),
     path("user_profile",views.user_profile,name='user_profile'),
-    path("update_password",views.update_password,name='update_password'),
     
 ]
 
